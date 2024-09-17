5009e68034cf19fee45b99e08b0834ec3c83e56c
yanbel0706 <yanis.belk.edu@gmail.com>
2024-09-17 14:18:49 +0100

    task resolved

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -3,12 +3,20 @@ import Car from './10-car';
 export default class EVCar extends Car {
   constructor(brand, motor, color, range) {
     super(brand, motor, color);
-    if (typeof _range === 'string') {
-      this._range = range;
-    }
+    this.range = range;
+  }
+
+  get range() {
+    return this._range;
+  }
+
+  set range(value) {
+    this._range = value;
   }
 
   cloneCar() {
-    return new Car(this._brand, this._motor, this._color); // Used `this` to access attributes
+    const Species = super.constructor[Symbol.species];
+
+    return new Species();
   }
 }
